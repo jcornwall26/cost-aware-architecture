d2f4b30a01268fdd6a55881cd230d0021691e040
Jason Cornwall <jason.s.cornwall@gmail.com>
2024-04-05 14:26:53 -0700

    refactor clean-up

---
+++ src/util/csv_writer.rs
@@ -0,0 +1,31 @@
+
+
+pub fn write_to_csv(lambda_name :&str, aws_profile: &str, aws_region: &str, com_results: Vec<(&str, f64, f64, f64, f64)>) {
+
+    let mut wtr = csv::Writer::from_path(format!("./{}-{}-{}-output.csv", &lambda_name, &aws_profile, &aws_region)).unwrap();
+
+    match wtr.write_record(&["function-name", "timestamp",  "monthly-cost", "invocations", "average-duration", "cost-per-100m-requests"]) {
+        Ok(_v) => (),
+        Err(e) => println!("{:?}", e)
+    }
+    match wtr.flush() {
+        Ok(_v) => (),
+        Err(_e) => ()
+    };
+
+    for com_result in com_results{
+        println!("{:?}", com_result);
+        match wtr.write_record(&[lambda_name, 
+            com_result.0, com_result.1.to_string().as_str(), 
+            com_result.2.to_string().as_str(), 
+            com_result.3.to_string().as_str(), 
+            com_result.4.to_string().as_str()]) {
+            Ok(_v) => (),
+            Err(e) => println!("{:?}", e)
+        }
+        match wtr.flush() {
+            Ok(_v) => (),
+            Err(_e) => ()
+        };
+    }
+}
